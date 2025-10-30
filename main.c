25375a81b7597be6f210eb6f543286a6580d4aa2
seonoleo <mscomy1@gmail.com>
2025-10-30 17:29:39 +0900

    09-5최종

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ void printMatrix(int A[][COLS])
     int i,j;
     for (i=0;i<ROWS;i++)
       {for (j=0;j<COLS;j++)
-       printf("%d",A[i][j]);
+       printf("%d ",A[i][j]);
       printf("\n");}
 }
 
