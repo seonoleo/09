42438fff809d742c25f62968678da064740bccf9
seonoleo <mscomy1@gmail.com>
2025-10-30 16:06:16 +0900

    09-3

--- main.c
+++ main.c
@@ -5,23 +5,21 @@
 
 int main(int argc, char *argv[])
 {
-  int i, average;
-  int sum=0;
-  int grade[SIZE];
-   
-    sum=0;
-
-   printf("%i명 점수를  입력하세요:\n",SIZE);
-
-   for(i=0;i<SIZE;i++)
+  int i;
+  int a[SIZE]={1,2,3,4,5};
+  int b[SIZE]={1,2,3,7,5};
+  int flag_same=1;
+  
+  for(i=0;i<SIZE;i++)
   { 
-    scanf("%d", &grade[i]);
-    sum+=grade[i];
-}
-    
-  average=sum/SIZE;
-  printf("성적 평균:%d\n",average);
-    
+    if(a[i]!=b[i])
+     {printf("%i-th element is diff\n",i);
+     flag_same=0;
+     }
+  }
+  
+  printf("Are a and b the same?: %i\n",flag_same);
+  
   system("PAUSE");	
   return 0;
 }
