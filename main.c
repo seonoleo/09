896987e92b09bbfeda78483dbf0632917496031b
seonoleo <mscomy1@gmail.com>
2025-10-30 15:47:34 +0900

    09-2 최종

--- main.c
+++ main.c
@@ -6,22 +6,22 @@
 int main(int argc, char *argv[])
 {
   int i, average;
-  int sum;
+  int sum=0;
   int grade[SIZE];
-  
-  sum=0;
-  
-  printf("%i명 점수를  입력하세요:\n",SIZE);
-  
-  for(i=0;i<SIZE;i++)
+   
+    sum=0;
+
+   printf("%i명 점수를  입력하세요:\n",SIZE);
+
+   for(i=0;i<SIZE;i++)
   { 
     scanf("%d", &grade[i]);
     sum+=grade[i];
-  
 }
+    
   average=sum/SIZE;
   printf("성적 평균:%d\n",average);
-   
+    
   system("PAUSE");	
   return 0;
 }
